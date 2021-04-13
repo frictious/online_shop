61a53bc1d02fbf69415eb870c8c47f6a665b95b7
frictious <gegbaid@gmail.com>
2021-04-13 04:11:35 +0000

    Started working on interface

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 //CONFIG
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 app.use("/", Index);
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ const   express             = require("express");
 
 const router = express.Router();
 
+//Home
 router.get("/", (req, res) => {
-    res.send("<h1>WELCOME TO THE HOME PAGE</h1>");
+    res.render("index", {title : "Online Shop Homepage"});
+});
+
+// About
+router.get("/about", (req, res) => {
+    res.render("about", {title : "About Online Shop"});
+});
+
+// Contact
+router.get("/contact", (req, res) => {
+    res.render("contact", {title : "Contact Online Shop"});
+});
+
+// Product
+router.get("/product", (req, res) => {
+    res.render("product", {title : "Online Shop Product Page"});
 });
 
 module.exports = router;
