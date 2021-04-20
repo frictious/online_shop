bebad431f413198b99c9a6534ed4ad65362546d3
frictious <gegbaid@gmail.com>
2021-04-20 07:16:30 +0000

    Started and finished contact us form logic

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const   express             = require("express"),
-        Product             = require("../models/products");
+        Product             = require("../models/products"),
+        nodemailer          = require("nodemailer");
 
 const router = express.Router();
+require("dotenv").config();
+
+// CONFIG
+const transport = nodemailer.createTransport({
+    service : "gmail",
+    auth:{
+        type: "login",
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
 
 //Home
 router.get("/", (req, res) => {
@@ -17,6 +29,49 @@ router.get("/about", (req, res) => {
 router.get("/contact", (req, res) => {
     res.render("contact", {title : "Contact Online Shop"});
 });
+
+// Sending Emails
+router.post("/contact", (req, res) => {
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.EMAIL,
+        subject : req.body.subject,
+        html: req.body.message
+    }
+
+    //Sending mail
+    transport.sendMail(mailOptions, (err, mail) => {
+        if(!err){
+            //Send mail to customer after sending us a mail using the contact form
+            const mailOptions = {
+                from: "slshop637@gmail.com",
+                to: req.body.email,
+                subject : `Thanks for contacting us`,
+                html: `<p>Dear <strong>${req.body.name}</strong>,</p>
+                <p>Hope this day has been going on well for you.</p>
+                <p>We want to thank you for reaching out to us.</p>
+                <p>A support staff has been notified of your mail, and will get back to you shortly.</p>
+                <p>Thank you for reaching out to us using our contact form.</p>
+                <br><br>
+                <p>Sincerely</p>
+                <p>SL Shop Team</p>`
+            }
+
+            //Sending mail
+            transport.sendMail(mailOptions, (err, mail) => {
+                if(!err){
+                    res.redirect("/contact");
+                }else{
+                    console.log(err);
+                    res.redirect("back");
+                }
+            });
+        }else{
+            console.log(err);
+            res.redirect("back");
+        }
+    });
+})
 
 // Product
 router.get("/product", (req, res) => {
